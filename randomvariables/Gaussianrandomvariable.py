c3f0943c3d1d2f5eb1d6d0f0dde19aa98a61e9a2
Brian Wang <brianwang92@gmail.com>
2019-05-07 08:03:18 -0700

    docs: Added docstrings to Gaussianrandomvariable methods, made small change to Exponential init docstring specifying that stdev must > 0

--- randomvariables/Gaussianrandomvariable.py
+++ randomvariables/Gaussianrandomvariable.py
@@ -4,18 +4,38 @@ from .Generalrandomvariable import RandomVariable, NonPositiveException, LessTha
 class Gaussian(RandomVariable):
 
 	def __init__(self, mu=0, sigma=1):
+		"""
+		Gaussian random variable class
+		
+		Attributes:
+		  mean (float) representing the mean value of the distribution
+		  stdev (float) must be > 0, representing the standard deviation of the distribution
+		"""
 		RandomVariable.__init__(self, mu, sigma)
 
 	def get_mean(self):
+		"""
+		Returns the mean attribute
+		"""
 		return self.mean
 
 	def get_variance(self):
+		"""
+		Returns the variance, which is the stdev attribute squared
+		"""
 		return self.stdev ** 2
 
 	def get_stdev(self):
+		"""
+		Returns the stdev attribute
+		"""
 		return self.stdev
 
 	def generate(self, n=1):
+		"""
+		@param n (int > 1) number of random numbers to generate from this random variable
+		@return a single randomly generated number (float) or a list of randomly generated floats
+		"""
 		if n < 1:
 			raise LessThanOneException('x must be greater than or equal to 1')
 		elif type(n) != int:
